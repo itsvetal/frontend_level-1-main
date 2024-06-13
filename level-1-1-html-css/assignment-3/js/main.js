40be52add405f00321329ae028b1c4cd16625dbc
iTsVetal <vitaliikryskiv.development@gmail.com>
2024-06-13 14:21:29 +0300

    Begin to make the carousel v1.5

--- level-1-1-html-css/assignment-3/js/main.js
+++ level-1-1-html-css/assignment-3/js/main.js
@@ -1,6 +1,6 @@
-function next() {
-    console.log("next");
-}
-function prev() {
-    console.log("prev");
-}
+// function next() {
+//     console.log("next");
+// }
+// function prev() {
+//     console.log("prev");
+// }
