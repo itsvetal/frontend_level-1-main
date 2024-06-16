5e120802aaf91603535d8593163d193f46bfc8cc
iTsVetal <vitaliikryskiv.development@gmail.com>
2024-06-16 12:09:00 +0300

    Made carousel with images  based on the Owl carousel 2. For this, the block 'intro' was completely redesigned. Work done with adaptation to different screen sizes for the 'services' and 'mansory' blocks. V1.1

--- level-1-1-html-css/assignment-3/js/main.js
+++ level-1-1-html-css/assignment-3/js/main.js
@@ -4,3 +4,24 @@
 // function prev() {
 //     console.log("prev");
 // }
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        navText: ["",""],
+        responsive: {
+            0: {
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+});
+
