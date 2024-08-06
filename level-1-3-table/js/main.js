106996cb1ef974292b1fe518b52749956fbf085b
iTsVetal <vitaliikryskiv.development@gmail.com>
2024-08-06 12:49:32 +0300

    Added comments to the data_table.js v1.5

--- level-1-3-table/js/main.js
+++ level-1-3-table/js/main.js
@@ -41,9 +41,8 @@ const users = [
     {id: 30050, name: 'Вася', surname: 'Петров', age: 12},
     {id: 30051, name: 'Вася', surname: 'Васечкін', age: 15},
     {id: 30052, name: 'Антон', surname: 'Іванов', age: 7},
-    {id: 30051, name: 'Оксана', surname: 'Крисків', age: 18},
-    {id: 30051, name: 'Григорій', surname: 'Ушаков', age: 35}
-
+    {id: 30053, name: 'Оксана', surname: 'Крисків', age: 18},
+    {id: 30054, name: 'Григорій', surname: 'Ушаков', age: 35}
 ];
 
 DataTable(config, users);
